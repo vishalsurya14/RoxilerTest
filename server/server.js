576ea70d4c377ce71988cfea7089aaa790e30792
Saurabh Ghiya <saurabhghiya@gmail.com>
2024-02-25 19:26:02 +0530

    server created with apis

---
+++ server/server.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+
+const apiRoutes = require('./routes/api');
+const mongoConnect = require('./db/mongoConnect');
+
+const app = express();
+
+mongoConnect();
+app.use(express.json());
+app.use(cors());
+
+app.use('/', apiRoutes);
+
+const PORT = process.env.PORT || 8080;
+const CLIENT_URL = process.env.CLIENT_URL || 'http://localhost:3000';
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
